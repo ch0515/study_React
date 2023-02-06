746ea7aa1de20e3e3307c0819750fb0e6dc797fb
chaemin <w2114@e-mirim.hs.kr>
2023-02-06 20:32:56 +0900

    삼항연산자

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import { Fragment } from 'react';
 
 function App() {
+  const name = '리액트';
   return (
-    <>
-      <h1>리액트 안녕!</h1>
-      <h2>잘 작동하니?</h2>
-    </>
+    <div>
+      {name === '액트' ? (
+        <h1>
+          리액트입니다.
+        </h1>
+      ):(
+        <h2>리액트가 아닙니다.</h2>
+      )}
+    </div>
   );
 }
 
