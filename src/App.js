7bf6c8ebbb65795e0db1e6bd2901a597aba624d2
chaemin <w2114@e-mirim.hs.kr>
2023-02-06 20:36:01 +0900

    && 연산자

--- src/App.js
+++ src/App.js
@@ -2,14 +2,7 @@
 function App() {
   const name = '리액트';
   return (
-    <div>
-      {name === '액트' ? 
-        <h1>
-          리액트입니다.
-        </h1>
-      : null
-      }
-    </div>
+    <div>{name === '리액트' && <h1>리액트입니다.</h1>}</div>
   );
 }
 
