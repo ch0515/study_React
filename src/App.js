a8878ff5f1729128bd3411cd2bed5070422d92ad
chaemin <w2114@e-mirim.hs.kr>
2023-02-06 20:34:27 +0900

    삼항연산자 null

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ function App() {
   const name = '리액트';
   return (
     <div>
-      {name === '액트' ? (
+      {name === '액트' ? 
         <h1>
           리액트입니다.
         </h1>
-      ):(
-        <h2>리액트가 아닙니다.</h2>
-      )}
+      : null
+      }
     </div>
   );
 }
