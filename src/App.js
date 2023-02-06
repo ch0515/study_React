a3df9cd597f03d3204b70189ce4e8801b9eaafc9
chaemin <w2114@e-mirim.hs.kr>
2023-02-06 20:43:09 +0900

    || 연산자

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
+import './App.css';
 
 function App() {
-  const number = 0;
-  return (
-    number && <div>내용</div>
-  );
+  const name = undefined;
+  return <div>{name || '리액트'}</div>
 }
 
 export default App;
