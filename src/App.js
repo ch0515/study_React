24ebf4e29a3d7201f3b495121bb941f2b4e187c8
ch0515 <w2114@e-mirim.hs.kr>
2023-02-11 00:41:56 +0900

    - 문제 카멜 표기법으로

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
 
 function App() {
-  const name = undefined;
-  return <div>{name || '리액트'}</div>
+  const name = '리액트';
+  const style = {
+    //background-color는 backgroundColor와 같이 -가 사라지고 카멜 표기법으로 작성
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontsize: '48px',
+    fontWeight: 'bold',
+    padding: 16 //단위 생략시 px 지정
+  };
+  return <div style={style}>{name} </div>;
 }
 
 export default App;
