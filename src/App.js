b311dc832858996a9bee6099ef1ad2f75f4cd56e
ch0515 <w2114@e-mirim.hs.kr>
2023-02-23 17:15:13 +0900

    style 값 선언하지 않고 지정하기

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
 
 function App() {
-  const name = '리액트';
-  const style = {
-    //background-color는 backgroundColor와 같이 -가 사라지고 카멜 표기법으로 작성
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontsize: '48px',
-    fontWeight: 'bold',
-    padding: 16 //단위 생략시 px 지정
-  };
-  return <div style={style}>{name} </div>;
+  const name='리액트';
+  return(
+    <div
+      style={{
+        backgroundColor: 'black',
+        color: 'aqua',
+        fontSize: '48px',
+        fontWeight: 'bold',
+        padding: 16
+      }}
+      >
+        {name}
+      </div>
+  );
 }
 
 export default App;
