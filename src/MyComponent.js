f1df6a98a5104b1f69ecb8dde157d04dce472b8a
chaemin <w2114@e-mirim.hs.kr>
2023-04-22 19:17:28 +0900

    props.children 사용하기

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,14 +1,14 @@
-const MyComponent = props => {
-    const {name, children} = props;
+const MyComponent = props =>{
     return(
         <div>
-        안녕하세요, 제 이름은 {name}입니다. <br/>
-        children의 값은 {children} 입니다.
+            안녕하세요, 제 이름은 {props.name} 입니다.<br/>
+            children 값은 {props.children}입니다.
         </div>
     );
 };
+
 MyComponent.defaultProps = {
-    name: '기본 이름'
+    name: '기본이름'
 };
 
-export default MyComponent; // -> 모듈 내보내기
+export default MyComponent;
