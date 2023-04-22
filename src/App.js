4d61859a8e5a46efed4030663379e763944b187d
chaemin <w2114@e-mirim.hs.kr>
2023-04-22 19:33:10 +0900

    클래스형 컴포넌트 state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import MyComponent from "./MyComponent";
+import Counter from "./Counter";
 
 const App = () => {
-  return <MyComponent name="React" favoriteNumber={7}>리액트</MyComponent>;
+  return <Counter/>;
 };
-
 export default App;
---
+++ src/Counter.js
@@ -0,0 +1,30 @@
+import { Component } from 'react';
+
+class Counter extends Component{
+    constructor(props){
+        super(props);
+        //state의 초깃값 설정하기
+        this.state = {
+            number: 0
+        };
+    }
+    render(){
+        const { number } = this.state; //state를 조회할때는 this.state로 조회
+        return(
+            <div>
+                <h1>{number}</h1>
+                <button
+                //onClick을 통해 버튼이 클릭되었을 때 호출할 함수 지정
+                onClick={() => {
+                    //this.setState를 사용해 state에 새로운 값 넣기
+                    this.setState({ number: number + 1});
+                }}
+                >
+                    +1
+                </button>
+            </div>
+        );
+    }
+}
+
+export default Counter;
