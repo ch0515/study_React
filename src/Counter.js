cba097ba8c8f267483298de287e0a3d04b6d4ebe
chaemin <w2114@e-mirim.hs.kr>
2023-05-07 17:10:14 +0900

    함수 인자 전달하기

--- src/Counter.js
+++ src/Counter.js
@@ -1,14 +1,10 @@
 import { Component } from 'react';
 
 class Counter extends Component{
-    constructor(props){
-        super(props);
-        //state의 초깃값 설정하기
-        this.state = {
-            number: 0,
-            fixedNumber: 0
-        };
-    }
+    state = {
+        number: 0,
+        fixedNumber: 0
+    };
     render(){
         const { number, fixedNumber } = this.state; //state를 조회할때는 this.state로 조회
         return(
@@ -19,7 +15,14 @@ class Counter extends Component{
                 //onClick을 통해 버튼이 클릭되었을 때 호출할 함수 지정
                 onClick={() => {
                     //this.setState를 사용해 state에 새로운 값 넣기
-                    this.setState({ number: number + 1});
+                    this.setState(prevState => {
+                        return{
+                            number: prevState.number + 1
+                        };
+                    });// 위, 아래 둘다 같은 기능을 함
+                    this.setState(prevState => ({
+                        number: prevState.number + 1
+                    }));
                 }}
                 >
                     +1
